5af3d00957e42bcf0ed71d007a37b1fe3f9fbe23
Hunter Hollis <hunterhollismobile@gmail.com>
2024-01-08 21:29:42 -0600

    Adding sub-folder, modify s1, delete s2

--- script01.py
+++ script01.py
@@ -1,2 +1,4 @@
 #this is for printing
 print("Hello world!")
+print("This is another line.")
+print("So is this.")
--- script02.py
+++
@@ -1,5 +0,0 @@
-#This is for defining
-def hello_world(message):
-    return message
-
-print(hello_world("Hello world!"))
